fd24371d2863d68ef97f68560ab9b972ac9d5d72
Manuelgph <manugp108@hotmail.com>
2024-11-07 12:35:33 -0300

    practicamos algorimos y pseucodigo sobre si una persona puede o no ingresar a una discoteca de acuerdo a su edad

---
+++ ingreso.permitido.py
@@ -0,0 +1,14 @@
+##paso1:Solicitar al usuario que ingrese la edad del cliente
+
+edad=int(input("Por favor ingrese su edad: "))
+         
+
+#Paso2:verificar si la edad ingresada cumple con el requisito para ingresar a la discoteca
+
+if edad>=21:
+    print("Usted si puede ingresar a la discoteca")
+
+else:
+    print("Usted No puede ingresar a la discoteca, usted es menor de edad")
+
+#Paso3:Mostrar al usuario si su cliente puede ingresar a la discoteca1
